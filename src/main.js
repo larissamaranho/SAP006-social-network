54bcaad8bccfd4bed4787dcdb0bc99f286681e32
larissamaranho <larissamaranhor@gmail.com>
2021-08-04 15:20:47 -0300

    routes and pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,46 @@
 // Este é o ponto de entrada da sua aplicação
 
-import { myFunction } from './lib/index.js';
+//import { myFunction } from './lib/index.js';
 
-myFunction();
+//myFunction();
+
+const email = 'larissamaranhor@gmail.com';
+const password = '123456';
+  //login novo usuario
+  
+/*firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log('Deu bom');
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log('Deu ruim', errorCode, errorMessage);
+  });*/
+
+
+  //login usuario existente
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+    console.log("logou");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message
+
+    console.log("nao logou");
+  });
+
+  //logout
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
---
+++ src/pages/feed/feed.js
@@ -0,0 +1,31 @@
+export const feed = () => {
+    const rootElement = document.createElement("div");
+    const container = `
+    <div>
+    <div>
+    <img id="background" src="./pages/login/img/paleta3.jpg" alt="">
+    <div class="esmaeceHeader logotipo-text">
+      <section>
+      <h2>As Doceiras</h2>
+    </section>
+  </div>
+    <h4>Não tem cadastro? <span><a href="/signup" id="signup">Cadastre-se</a></span></h4>
+    <div class="inputAndReset">
+    <input type="text" id="email" class="input" placeholder="Email">
+    <input type="password" id="password" class="input" placeholder="Senha">
+    <a class="reset-password" id="reset" href="/reset">Esqueceu a senha?</a><br>
+  </div>
+  <div class="google">
+    <button id="btn-login" class="login btn">LOGIN</button>
+    <img id="icon-google" src="./pages/login/img/icon-google-white.png">
+  </div>
+  <form>
+    <input type="checkbox" class="checkbox" name="remember"><label for="remember">Lembrar meus dados</label>
+  </form>
+
+    </div> 
+    `;
+
+    rootElement.innerHTML = container;
+    return rootElement;
+};
