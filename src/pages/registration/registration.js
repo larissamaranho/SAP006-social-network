901ae22a00e0b9a2159e8790e4167a4e7bcf1ac7
larissamaranho <larissamaranhor@gmail.com>
2021-08-12 15:26:02 -0300

    add images

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -8,14 +8,13 @@ export const registration = () => {
           <h2>As Doceiras</h2>
         </section>
       </div>
-        
         <div class="inputAndReset">
         <input type="text" id="nome" class="nome" placeholder="Nome">
         <input type="text" id="sobrenome" class="sobrenome" placeholder="Sobrenome">
         <input type="email" id="email" class="email" placeholder="E-mail">
         <input type="password" id="password" class="input" placeholder="Senha">
         <input type="password" id="password" class="input" placeholder="Confirme sua senha">
-        <h4>Ja tem cadastro? <span><a href="/signup" id="signup">Faça o login</a></span></h4>
+        <h4>Ja tem cadastro? <span><a href="/signup" id="signup">Faça o login.</a></span></h4>
       </div>
       <div class="google">
         <button id="btn-login" class="login btn">LOGIN</button>
